c6dae0de0790166eb6c478fbf5f63e9f97504282
root <chinenyediei@gmail.com>
2019-12-03 19:18:38 -0600

    typing effect working, but not stopping

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 
+var count = 0; 
+var index = 0;
+var currentText = '';
+var letter = '';
+
+(function type() {
+  
+  const texts = ['am a Full Stack Web Developer', 'love solving problems', 'beautiful front end design'];
+  
+  if (count === texts.length) {
+    count = 0;
+  }
+
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+  document.querySelector('.typing').textContent = letter; 
+  
+  if (letter.length === currentText.length) {
+    count++; 
+    index = 0; 
+  }
+  setTimeout(type, 400); 
+}());
+
+
+
+
+ 
