8ea08b686a7ee2dace80bce153ccdab76c78df3b
root <chinenyediei@gmail.com>
2020-02-27 19:29:43 -0600

    updated formating and added to about me page

--- js/app.js
+++ js/app.js
@@ -1,27 +1,5 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-//typing effect
-
-<button onclick="move()">Click Me</button> 
-
-
-var i = 0;
-function move() {
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-      }
-    }
-  }
-}
+$(document).ready(function () {
+  console.log("ready!");
+  //typing effect
+  0
 });
-
--- js/progress.js
+++ js/progress.js
@@ -1,28 +1,4 @@
-.ui-progressbar {
-    position: relative;
-  }
-  #progressbar {
-    background: #f5fffa;
-  }
-  .ui-progressbar .ui-progressbar-value {
-    margin: 0;
-  }
-  .progress-label {
-    position: absolute;
-    text-align: center;
-    width: 100%;
-    color: #cccccc;
-    font-family: 'Open Sans Condensed', sans-serif;
-    font-size: 20px;
-  }
-  .ui-widget-header {
-    background: #ec2528;
-  }
-  p {
-    text-align: center;
-    font-family: 'Open Sans Condensed', sans-serif;
-    font-size: 26px;
-  }
-  .well {
-    margin-top: 20px;
-  }
+var progressBar = require(progressbar.js); 
+
+var bar = new ProgressBar.Line('#progressBar', {easing: 'easeInOut'});
+bar.animate(1);  // Value from 0.0 to 1.0
