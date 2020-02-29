e3be5860e34b7afd02bf1988addad3014f35b30e
root <chinenyediei@gmail.com>
2020-02-29 07:19:48 -0600

    updated card with border and image, need to position picture

--- js/app.js
+++ js/app.js
@@ -1,5 +1,42 @@
-$(document).ready(function () {
+
+var i = 0;
+var txt = 'Lorem ipsum dummy text blabla.';
+var speed = 50;
+
+$(document).ready(function() {
   console.log("ready!");
-  //typing effect
-  
+  function typeWriter() {
+    if (i < txt.length) {
+      $("#app").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+  $(".btn").click(function () {
+    console.log("clicked");
+    
+  });
 });
+$(document).ready(function () {
+  // Listen to submit event on the <form> itself!
+  $('#main').submit(function (e) {
+
+    e.preventDefault();
+
+    var name = $("#name").val();
+    var email = $("#email").val();
+
+    $.post("process.php", {
+      name: name,
+      email: email
+    }).complete(function() {
+        console.log("Success");
+      });
+  });
+});
+
+  
+
+
+ 
+
