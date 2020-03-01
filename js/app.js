06dab1e16aa2dc24d8ffc11b6f712c116feb1b6a
root <chinenyediei@gmail.com>
2020-02-29 19:30:22 -0600

    centered header on knowledge page

--- js/app.js
+++ js/app.js
@@ -1,38 +1,19 @@
-
-var i = 0;
-var txt = 'Lorem ipsum dummy text blabla.';
-var speed = 50;
+var names = "";
+var email = "";
+var message = '';
 
 $(document).ready(function() {
-  console.log("ready!");
-  function typeWriter() {
-    if (i < txt.length) {
-      $("#app").innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
-    }
-  }
-  $(".btn").click(function () {
-    console.log("clicked");
-    
-  });
-});
-$(document).ready(function () {
-  // Listen to submit event on the <form> itself!
-  $('#main').submit(function (e) {
 
+  $(".form-group").submit(function (e) {
+
+    //prevent Default functionality
     e.preventDefault();
-
-    var name = $("#name").val();
-    var email = $("#email").val();
-
-    $.post("process.php", {
-      name: name,
-      email: email
-    }).complete(function() {
-        console.log("Success");
-      });
+    names = $("#names").val();
+    email = $("#email").val();
+    message = $("#message").val();
+    console.log("submitted"); 
   });
+  
 });
 
   
